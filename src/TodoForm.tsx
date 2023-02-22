b1992e38ff84c37ef2ebeed319ce62af588fc4db
Alex Linden <alextl2004@gmail.com>
2023-02-21 16:00:54 -0800

    type todoform and functions

--- src/TodoForm.tsx
+++ src/TodoForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { TodoFormInterface } from "./interfaces";
 
 const defaultInitialFormData = { title: "", description: "", priority: 1 };
 
@@ -11,11 +12,16 @@ const defaultInitialFormData = { title: "", description: "", priority: 1 };
  * { TodoApp, EditableTodo } -> TodoForm
  */
 
-function TodoForm({ initialFormData = defaultInitialFormData, handleSave }) {
+interface TodoFormData {
+  initialFormData?: TodoFormInterface;
+  handleSave: (formData: TodoFormInterface) => void;
+}
+
+function TodoForm({ initialFormData = defaultInitialFormData, handleSave }: TodoFormData): JSX.Element {
   const [formData, setFormData] = useState(initialFormData);
 
   /** Update form input. */
-  function handleChange(evt) {
+  function handleChange(evt: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>): void {
     const input = evt.target;
     setFormData(formData => ({
       ...formData,
@@ -24,61 +30,61 @@ function TodoForm({ initialFormData = defaultInitialFormData, handleSave }) {
   }
 
   /** Call parent function and clear form. */
-  function handleSubmit(evt) {
+  function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
     evt.preventDefault();
     handleSave(formData);
     setFormData(initialFormData);
   }
 
   return (
-      <form className="NewTodoForm" onSubmit={handleSubmit}>
+    <form className="NewTodoForm" onSubmit={handleSubmit}>
 
-        <div className="mb-3">
-          <input
-              id="newTodo-title"
-              name="title"
-              className="form-control"
-              placeholder="Title"
-              onChange={handleChange}
-              value={formData.title}
-              aria-label="Title"
-          />
+      <div className="mb-3">
+        <input
+          id="newTodo-title"
+          name="title"
+          className="form-control"
+          placeholder="Title"
+          onChange={handleChange}
+          value={formData.title}
+          aria-label="Title"
+        />
+      </div>
+
+      <div className="mb-3">
+        <textarea
+          id="newTodo-description"
+          name="description"
+          className="form-control"
+          placeholder="Description"
+          onChange={handleChange}
+          value={formData.description}
+          aria-label="Description"
+        />
+      </div>
+
+      <div className="mb-3">
+        <div className="w-75 d-flex justify-content-between">
+          <label htmlFor="newTodo-priority"
+            className="d-inline-flex">Priority:&nbsp;&nbsp;
+          </label>
+          <select id="newTodo-priority"
+            name="priority"
+            value={formData.priority}
+            onChange={handleChange}
+            className="form-control form-control-sm d-inline-flex"
+          >
+            <option value={1}>Ultra-Über</option>
+            <option value={2}>Über</option>
+            <option value={3}>Meh</option>
+          </select>
         </div>
+        <button className="btn-primary btn btn-sm NewTodoForm-addBtn">
+          Gø!
+        </button>
+      </div>
 
-        <div className="mb-3">
-          <textarea
-              id="newTodo-description"
-              name="description"
-              className="form-control"
-              placeholder="Description"
-              onChange={handleChange}
-              value={formData.description}
-              aria-label="Description"
-          />
-        </div>
-
-        <div className="mb-3">
-          <div className="w-75 d-flex justify-content-between">
-            <label htmlFor="newTodo-priority"
-                   className="d-inline-flex">Priority:&nbsp;&nbsp;
-            </label>
-            <select id="newTodo-priority"
-                    name="priority"
-                    value={formData.priority}
-                    onChange={handleChange}
-                    className="form-control form-control-sm d-inline-flex"
-            >
-              <option value={1}>Ultra-Über</option>
-              <option value={2}>Über</option>
-              <option value={3}>Meh</option>
-            </select>
-          </div>
-          <button className="btn-primary btn btn-sm NewTodoForm-addBtn">
-            Gø!
-          </button>
-        </div>
-
-      </form>
+    </form>
   );
 }
 
